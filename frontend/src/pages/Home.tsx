7cff87d1876f3ce9f489f0c19b89141028514252
RocioT08 <fannytutillo@gmail.com>
2026-02-08 16:21:06 -0500

    Migration from fronend to react

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,21 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 32 }}>
+      <h1>Educational Investment Platform 📈</h1>
+
+      <p>This tool helps you understand financial risk and forecasting.</p>
+
+      <ul>
+        <li>Single Asset View</li>
+        <li>Comparison View</li>
+      </ul>
+
+      <p>
+        Find tickers on{" "}
+        <a href="https://ca.finance.yahoo.com/" target="_blank">
+          Yahoo Finance
+        </a>
+      </p>
+    </div>
+  );
+}
